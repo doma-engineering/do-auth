5fe9798d0b7c78da495b5e252252688ac41c308d
conflagrate <jm@memorici.de>
2022-03-22 00:29:45 +0000

    Problem: No way to run one-shot actions against backend
    Solution:
    
    In order to run wdio suites, it would be cool to be able to run one-shot tests against the backend in question.
    
    We implemented it by making sure we have a production build built in Dockerfile. We removed the "ENDPOINT" directive, because, really, it's fairly useless.
    
    Then we used netcat (which is included in today's version of sneakium) in order to have a simple single-message passing protocol between containers so that they know when to do certain actions. This is a huge change to docker-compose.yaml.
    
    We changed run.ts to allow for other react-script verbs (not only start) and reformatted it.

--- priv/ui/run.ts
+++ priv/ui/run.ts
@@ -6,30 +6,40 @@ import child_process from 'child_process';
 const main = () => {
     const userSuppliedArgs = parseArgs();
     const listing = fs.readdirSync('./src');
-    const entryPoints = (listing.reduce((acc, x) => {
+    const entryPoints = listing.reduce((acc, x) => {
         if (isTsxEntry(x)) {
             return [x, ...acc];
         }
         return acc;
-    }, []));
-    if (userSuppliedArgs.length === 0 || !entryPoints.includes(userSuppliedArgs[0])) {
+    }, []);
+    if (
+        userSuppliedArgs.length === 0 ||
+        !entryPoints.includes(userSuppliedArgs[0])
+    ) {
         return console.log(
-            "Please supply one of the following entry points:", entryPoints
+            'Please supply one of the following entry points:',
+            entryPoints
         );
     }
-    const ph = child_process.spawn('npm', ['start'], { env: { ...process.env, REACT_APP_ENTRY: userSuppliedArgs[0] } });
+    var verb = 'start';
+    if (userSuppliedArgs.length >= 2) {
+        verb = userSuppliedArgs[2 - 1];
+    }
+    const ph = child_process.spawn('npm', ['run', verb], {
+        env: { ...process.env, REACT_APP_ENTRY: userSuppliedArgs[1 - 1] },
+    });
     ph.stdout.on('data', (x) => console.log(x.toString()));
-}
+};
 
 const parseArgs = (): string[] => {
     return process.argv.slice(2);
-}
+};
 
 const startsWithUpcase = (w: string) => w[0] === w[0].toUpperCase();
 
 const isTsxEntry = (x: string): boolean => {
     const bits = x.split('.');
     return bits.length === 2 && startsWithUpcase(bits[0]) && bits[1] === 'tsx';
-}
+};
 
 main();
