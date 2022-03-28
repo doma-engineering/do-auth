78507cde971c7900f9eb960251395a02cd0fb267
conflagrate <jm@memorici.de>
2022-03-28 19:30:01 +0100

    Problem: Formatting is horrid
    Solution:
    
     - Autoformat with prettier

--- priv/ui/config-overrides.js
+++ priv/ui/config-overrides.js
@@ -1,19 +1,21 @@
 module.exports = function override(config, env) {
-
-		okv = {
-          fallback: {
-								path: require.resolve("path-browserify"),
-								crypto: require.resolve("crypto-browserify"),
-								stream: require.resolve("stream-browserify"),
-								buffer: require.resolve("buffer")
-					},
+    okv = {
+        fallback: {
+            path: require.resolve('path-browserify'),
+            crypto: require.resolve('crypto-browserify'),
+            stream: require.resolve('stream-browserify'),
+            buffer: require.resolve('buffer'),
+        },
     };
 
     if (!config.resolve) {
         config.resolve = okv;
     } else {
-        config.resolve.fallback = { ...config.resolve.fallback, ...okv.fallback };
-		}
+        config.resolve.fallback = {
+            ...config.resolve.fallback,
+            ...okv.fallback,
+        };
+    }
 
     return config;
-}
+};
