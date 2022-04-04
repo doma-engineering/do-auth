b9378113f4e49e82f87bfc614c45f0d32282fabc
conflagrate <jm@memorici.de>
2022-04-04 16:15:19 +0100

    Problem: No way to run "simple" mocha tests
    Solution:
    
     - Add mocha tests
     - Make sure that npm test is a one-shot operation
     - Add fast check for property based testing
     - Verify it works

--- priv/ui/wdio.conf.js
+++ priv/ui/wdio.conf.js
@@ -20,7 +20,7 @@ exports.config = {
     // then the current working directory is where your `package.json` resides, so `wdio`
     // will be called from there.
     //
-    specs: ['./test/*.js'],
+    specs: ['./test/wdio/*.js'],
     // Patterns to exclude.
     exclude: [
         // 'path/to/excluded/files'
--- test/invite_test.exs
+++ test/invite_test.exs
@@ -1,53 +1,51 @@
 defmodule DoAuth.InviteTest do
-
   alias Uptight.Base, as: B
 
-	@moduledoc """
-	Tests of invite logic and that authed endpoints work
-	"""
-	use Plug.Test
-	use ExUnit.Case, async: true
+  @moduledoc """
+  Tests of invite logic and that authed endpoints work
+  """
+  use Plug.Test
+  use ExUnit.Case, async: true
 
-	use DoAuth.TestFixtures, [:crypto]
-	alias DoAuth.{Crypto, Invite}
-	alias Uptight.Result
+  use DoAuth.TestFixtures, [:crypto]
+  alias DoAuth.{Crypto, Invite}
+  alias Uptight.Result
 
-	test "root invitations can be granted" do
-		granted = Invite.grant_root_invite() |> Result.from_ok()
-		assert Crypto.verify_map(granted) |> Result.is_ok?()
-		assert Crypto.verify_map(granted["verifiableCredential"]) |> Result.is_ok?()
-	end
+  test "root invitations can be granted" do
+    granted = Invite.grant_root_invite() |> Result.from_ok()
+    assert Crypto.verify_map(granted) |> Result.is_ok?()
+    assert Crypto.verify_map(granted["verifiableCredential"]) |> Result.is_ok?()
+  end
 
-	test "root invites can be fulfilled" do
-		granted = Invite.grant_root_invite() |> Result.from_ok()
-		kp1 = signing_key_fixture(Enum.random(1..4)) |> map(&B.safe!/1)
-		fulfilled = Invite.fulfill(kp1[:public], granted) |> Result.from_ok()
-		assert Crypto.verify_map(fulfilled) |> Result.is_ok?()
-		assert Invite.lookup(kp1[:public]) == fulfilled
-	end
+  test "root invites can be fulfilled" do
+    granted = Invite.grant_root_invite() |> Result.from_ok()
+    kp1 = signing_key_fixture(Enum.random(1..4)) |> map(&B.safe!/1)
+    fulfilled = Invite.fulfill(kp1[:public], granted) |> Result.from_ok()
+    assert Crypto.verify_map(fulfilled) |> Result.is_ok?()
+    assert Invite.lookup(kp1[:public]) == fulfilled
+  end
 
-	test "invites can not be reused" do
-		granted = Invite.grant_root_invite() |> Result.from_ok()
+  test "invites can not be reused" do
+    granted = Invite.grant_root_invite() |> Result.from_ok()
 
-		sig_granted = granted["proof"]["signature"]
-		sig_root_invite = granted["verifiableCredential"]["proof"]["signature"]
+    sig_granted = granted["proof"]["signature"]
+    sig_root_invite = granted["verifiableCredential"]["proof"]["signature"]
 
-		fulfs = GenServer.call(DoAuth.Invite, {:get_fulfillments, sig_root_invite})
-		assert 0 < fulfs |> Enum.count()
-		fulfs = GenServer.call(DoAuth.Invite, {:get_fulfillments, sig_granted})
-		assert 0 == fulfs |> Enum.count()
+    fulfs = GenServer.call(DoAuth.Invite, {:get_fulfillments, sig_root_invite})
+    assert 0 < fulfs |> Enum.count()
+    fulfs = GenServer.call(DoAuth.Invite, {:get_fulfillments, sig_granted})
+    assert 0 == fulfs |> Enum.count()
 
-		kp1 = signing_key_fixture(14) |> map(&B.safe!/1)
-		kp2 = signing_key_fixture(15) |> map(&B.safe!/1)
-		# Fulfill me once, shame on me...
-		_fulfilled = Invite.fulfill(kp1[:public], granted) |> Result.from_ok()
+    kp1 = signing_key_fixture(14) |> map(&B.safe!/1)
+    kp2 = signing_key_fixture(15) |> map(&B.safe!/1)
+    # Fulfill me once, shame on me...
+    _fulfilled = Invite.fulfill(kp1[:public], granted) |> Result.from_ok()
 
-		fulfs = GenServer.call(DoAuth.Invite, {:get_fulfillments, sig_granted})
-		assert 1 == fulfs |> Enum.count()
+    fulfs = GenServer.call(DoAuth.Invite, {:get_fulfillments, sig_granted})
+    assert 1 == fulfs |> Enum.count()
 
-		# But you can't fulfill me twice. It's just not how it works.
-		fulfilled2 = Invite.fulfill(kp2[:public], granted)
-		assert fulfilled2 |> Result.is_err?()
-	end
-
+    # But you can't fulfill me twice. It's just not how it works.
+    fulfilled2 = Invite.fulfill(kp2[:public], granted)
+    assert fulfilled2 |> Result.is_err?()
+  end
 end
