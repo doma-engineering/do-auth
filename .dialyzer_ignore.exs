19aa23425535080148d64c10c68f8c80c1b12237
conflagrate <jm@memorici.de>
2022-05-19 22:55:44 +0100

    Problem: ExUnit isn't packaged for prod
    Solution:
    
     - For the time being, don't trigger dialyzer for unknown functions.
    
    Further work:
    
     - Port ExUnit.Assertions to Uptight

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,4 +1,4 @@
 [
   # Function ExUnit.Assertions.assert/2 does not exist.
-  {":0:unknown_function Function ExUnit.Assertions.assert/2 does not exist.", :unknown_function},
+  # {".*Function ExUnit.Assertions.assert/2 does not exist.*", :unknown_function}
 ]
