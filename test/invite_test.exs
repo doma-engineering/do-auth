a11cde77d4a9f54461261c7c2e271f84a0c5c9fb
conflagrate <jm@memorici.de>
2022-05-26 14:02:20 +0100

    Problem: Tests crash due to launch nondeterminism
    Solution:
    
     - Prove that ExUnit doesn't guarantee tests being ran from top to bottom with IO.inspect
     - Unify fulfill & reuse tests to add determinism

--- test/invite_test.exs
+++ test/invite_test.exs
@@ -12,20 +12,22 @@ defmodule DoAuth.InviteTest do
   alias Uptight.Result
 
   test "root invitations can be granted" do
+    # IO.inspect("ROOT INVITATIONS TEST START")
     granted = Invite.grant_root_invite() |> Result.from_ok()
     assert Crypto.verify_map(granted) |> Result.is_ok?()
     assert Crypto.verify_map(granted["verifiableCredential"]) |> Result.is_ok?()
+    # IO.inspect("ROOT INVITATIONS TEST END")
   end
 
-  test "root invites can be fulfilled" do
+  test "root invites can be fulfilled and cannot be reused" do
+    # IO.inspect("FULFILL TEST START")
     granted = Invite.grant_root_invite() |> Result.from_ok()
     kp1 = signing_key_fixture(Enum.random(1..4)) |> map(&B.safe!/1)
     fulfilled = Invite.fulfill(kp1[:public], granted) |> Result.from_ok()
     assert Crypto.verify_map(fulfilled) |> Result.is_ok?()
     assert Invite.lookup(kp1[:public]) == fulfilled
-  end
-
-  test "invites can not be reused" do
+    # IO.inspect("FULFILL TEST END")
+    # IO.inspect("REUSE TEST START")
     granted = Invite.grant_root_invite() |> Result.from_ok()
 
     sig_granted = granted["proof"]["signature"]
@@ -47,5 +49,6 @@ defmodule DoAuth.InviteTest do
     # But you can't fulfill me twice. It's just not how it works.
     fulfilled2 = Invite.fulfill(kp2[:public], granted)
     assert fulfilled2 |> Result.is_err?()
+    # IO.inspect("REUSE TEST END")
   end
 end
