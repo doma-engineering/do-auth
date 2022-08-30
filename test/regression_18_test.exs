22a982b0b2503368ae82e331c4e5729146bb0d62
conflagrate <jm@memorici.de>
2022-08-30 10:56:43 +0100

    Add forgotten test.

---
+++ test/regression_18_test.exs
@@ -0,0 +1,18 @@
+defmodule DoAuth.Regression18Test do
+  use Plug.Test
+  use ExUnit.Case, async: true
+
+  use DoAuth.TestFixtures, [:crypto]
+  alias DoAuth.{Crypto, Credential}
+  alias Uptight.Result
+
+  test "verify map can fail on invalid credential" do
+    valid_credential =
+      Credential.mk_credential!(signing_key_fixture(10), %{"untampered" => "qwerty"})
+
+    invalid_subject = valid_credential["credentialSubject"] |> Map.replace("untampered", "asd")
+    invalid_credential = valid_credential |> Map.replace("credentialSubject", invalid_subject)
+    result = Crypto.verify_map(invalid_credential)
+    assert Crypto.verify_map(invalid_credential) |> Result.is_err?()
+  end
+end
